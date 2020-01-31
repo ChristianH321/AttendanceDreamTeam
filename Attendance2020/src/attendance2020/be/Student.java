c659b4de83a4edb948e4e1f1732772e9a6a200b3
dpankov91 <dpankov91@gmail.com>
2020-01-31 10:20:31 +0100

    Student class

---
+++ Attendance2020/src/attendance2020/be/Student.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendance2020.be;
+
+import javafx.beans.property.StringProperty;
+
+/**
+ *
+ * @author hp
+ */
+public class Student 
+{
+    
+    private StringProperty userName;
+    private StringProperty userPassword;
+
+    public Student(StringProperty userName, StringProperty userPassword) 
+    {
+        this.userName = userName;
+        this.userPassword = userPassword;
+    }
+
+    public StringProperty getUserName() {
+        return userName;
+    }
+
+    public StringProperty getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserName(StringProperty userName) {
+        this.userName = userName;
+    }
+
+    public void setUserPassword(StringProperty userPassword) {
+        this.userPassword = userPassword;
+    }
+    
+    
+    
+    
+    
+}
